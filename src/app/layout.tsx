3aa032c4777e2f72259a47000a4585594b3016b1
Kusal Kalinga <kusalkalingainfo@gmail.com>
2026-01-03 13:34:04 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,43 @@ import Footer from "@/components/Layout/Footer";
 import { ThemeProvider } from "next-themes";
 import ScrollToTop from "@/components/ScrollToTop";
 import { ToastContainer } from "react-toastify";
+import { Metadata } from "next";
 const dmsans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "My365Expert | Microsoft 365 Security, Compliance & AI Experts",
+  description:
+    "Secure, optimise, and future-proof your Microsoft 365 environment. My365Expert helps businesses improve security, compliance, and AI readiness. Free consultation available.",
+  icons: {
+    icon: [{ url: "/favicon.ico" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "My365Expert | Microsoft 365 Security, Compliance & AI Experts",
+    description:
+      "Secure, optimise, and future-proof your Microsoft 365 environment. My365Expert helps businesses improve security, compliance, and AI readiness. Free consultation available.",
+    images: ["/images/social/og-image.png"],
+  },
+  openGraph: {
+    title: "My365Expert | Microsoft 365 Security, Compliance & AI Experts",
+    description:
+      "Secure, optimise, and future-proof your Microsoft 365 environment. My365Expert helps businesses improve security, compliance, and AI readiness. Free consultation available.",
+    images: [
+      {
+        url: "/images/social/og-image.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/images/social/og-image.png",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
